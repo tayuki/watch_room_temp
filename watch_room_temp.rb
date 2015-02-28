6b64e7246e0271fed10088bfc360d52b60d5750d
tayuki <maclife.taka@gmail.com>
2015-02-28 11:11:45 +0900

    add message & to slack func & use dotenv

--- watch_room_temp.rb
+++ watch_room_temp.rb
@@ -1,7 +1,29 @@
-require 'slack'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'dotenv'
 
-Slack.chat_postMessage(text: '‰·“xŠÄŽ‹‚ð‚Í‚¶‚Ü‚ß‚·:cop:', channel: 'general')
+Dotenv.load
 
+def post(text)
+  data = {
+    "text" => text
+  }
+  uri = URI.parse(ENV[REQ_URL])
+  http = Net::HTTP.post_form(uri, {"payload" => data.to_json})
+end
+
+post("¿¿/¿¿¿¿¿¿¿¿¿¿¿¿:cop:")
+
+#set alert temp
+cold_temp = 20
+hot_temp = 28
+
+#set alert hum
+high_hum = 75
+low_hum = 35
+
+#init status
 temp_clean = true
 hum_clean = true
 
@@ -10,11 +32,13 @@ loop do
   data_array = data.split(",")
 
   if temp_clean
-    if data[0].to_i < 20
+    if data[0].to_i < cold_temp
       #send cool nofity messege
+      post(":snowflake:¿¿¿#{cold_temp}¿¿¿¿¿¿¿¿¿")
       temp_clean = false
-    elsif data[0].to_i > 28
+    elsif data[0].to_i > hot_temp
       #send hot notify message
+      post(":sunny:¿¿¿#{hot_temp}¿¿¿¿¿¿¿¿¿")
       temp_clean = false
     else
       temp_clean = true
@@ -23,11 +47,13 @@ loop do
 
 
   if hum_clean
-    if data[1].to_i < 35
+    if data[1].to_i < low_hum
       #send dry nofity messege
+      post(":cactus:¿¿¿#{low_hum}¿¿¿¿¿¿¿¿")
       hum_clean = false
-    elsif data[1].to_i > 70
+    elsif data[1].to_i > high_hum
       #send wet notify message
+      post(":droplet:¿¿¿#{high_hum}¿¿¿¿¿¿¿¿")
       hum_clean = false
     else
       hum_clean = true
