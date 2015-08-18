718cec2a6eb80cf60f3e725135e0bdcc33f57bd1
tayuki <maclife.taka@gmail.com>
2015-08-18 09:48:09 +0900

    Add tail func

--- watch_room_temp.rb
+++ watch_room_temp.rb
@@ -14,6 +14,7 @@ def post(text)
 end
 
 post("温度/湿度を監視はじめます:cop:")
+system('~/aquestalkpi/AquesTalkPi "温度、湿度を監視始めます" | aplay')
 
 #set alert temp
 cold_temp = 18
@@ -27,6 +28,13 @@ low_hum = 30
 temp_clean = true
 hum_clean = true
 
+def get_data
+  data = `sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4`
+  unless data == "Failed to get reading. Try again!"
+    data_array = data.split(",")
+  end
+end
+
 loop do
   data = `sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4`
   data_array = data.split(",")
@@ -34,10 +42,12 @@ loop do
   if data_array[0].to_i < cold_temp
     #send cool nofity messege
     post(":snowflake:温度が#{cold_temp}度以下になりました") if temp_clean
+    system('~/aquestalkpi/AquesTalkPi "温度が#{cold_temp}度以下になりました" | aplay') if temp_clean
     temp_clean = false
   elsif data_array[0].to_i > hot_temp
     #send hot notify message
     post(":sunny:温度が#{hot_temp}度以上になりました") if temp_clean
+    system('~/aquestalkpi/AquesTalkPi "温度が#{hot_temp}度以以上になりました" | aplay') if temp_clean
     temp_clean = false
   else
     temp_clean = true
