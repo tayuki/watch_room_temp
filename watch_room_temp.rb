1cfd95f2faac5bf5524dee5ee0de54870016f540
tayuki <maclife.taka@gmail.com>
2015-08-29 23:49:41 +0900

    Add say method

--- watch_room_temp.rb
+++ watch_room_temp.rb
@@ -13,8 +13,21 @@ def post(text)
   http = Net::HTTP.post_form(uri, {"payload" => data.to_json})
 end
 
+def get_data
+  data = `sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4`
+  unless data == "Failed to get reading. Try again!"
+    data_array = data.split(",")
+  end
+end
+
+def say(serif)
+  command = "~/aquestalkpi/AquesTalkPi #{serif} | aplay"
+  system(command)
+end
+
+
 post("温度/湿度を監視はじめます:cop:")
-system('~/aquestalkpi/AquesTalkPi "温度、湿度を監視始めます" | aplay')
+say("温度、湿度を監視始めます")
 
 #set alert temp
 cold_temp = 18
@@ -28,12 +41,6 @@ low_hum = 30
 temp_clean = true
 hum_clean = true
 
-def get_data
-  data = `sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4`
-  unless data == "Failed to get reading. Try again!"
-    data_array = data.split(",")
-  end
-end
 
 loop do
   data = `sudo /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 2302 4`
@@ -41,13 +48,17 @@ loop do
 
   if data_array[0].to_i < cold_temp
     #send cool nofity messege
-    post(":snowflake:温度が#{cold_temp}度以下になりました") if temp_clean
-    system('~/aquestalkpi/AquesTalkPi "温度が#{cold_temp}度以下になりました" | aplay') if temp_clean
+    if temp_clean
+      post(":snowflake:温度が#{cold_temp}度以下になりました")
+      say("温度が#{cold_temp}度以下になりました")
+    end
     temp_clean = false
   elsif data_array[0].to_i > hot_temp
     #send hot notify message
-    post(":sunny:温度が#{hot_temp}度以上になりました") if temp_clean
-    system('~/aquestalkpi/AquesTalkPi "温度が#{hot_temp}度以以上になりました" | aplay') if temp_clean
+    if temp_clean
+      post(":sunny:温度が#{hot_temp}度以上になりました")
+      say("温度が#{hot_temp}度以上になりました")
+    end
     temp_clean = false
   else
     temp_clean = true
